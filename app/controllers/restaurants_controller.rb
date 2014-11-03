3ae853eacbd696ff979395266f440f5cc78db149
Anna Jerofejeva <jerofejeva.anna@yahoo.com>
2014-11-03 15:52:48 +0000

    Can now update restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,11 +8,21 @@ class RestaurantsController < ApplicationController
 	end
 
 	def create
-		Restaurant.create(params[:restaurant].permit(:name))
+		Restaurant.create(params[:restaurant].permit(:name, :description))
 		redirect_to '/restaurants'
 	end
 
 	def show
 		@restaurant = Restaurant.find(params[:id])
+	end
+
+	def edit
+		@restaurant = Restaurant.find(params[:id])
+	end
+
+	def update
+		@restaurant = Restaurant.find(params[:id])
+		@restaurant.update(params[:restaurant].permit(:name))
+		redirect_to '/restaurants'
 	end
 end
